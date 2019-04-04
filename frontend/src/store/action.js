981d45ff6a4b4fd1b2b2d508c09a216033eb1e1a
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-04-04 17:55:25 +0600

    link with href added to app.js

--- frontend/src/store/action.js
+++ frontend/src/store/action.js
@@ -3,18 +3,7 @@ import axios from "../axios-api";
 export const FETCH_SUCCESS ='FETCH_SUCCESS';
 export const CREATE_LINK_SUCCESS = "CREATE_LINK_SUCCESS";
 
-export const fetchSuccess = data => {
-    return {type: FETCH_SUCCESS, data};
-};
 export const createLinkSuccess = data => ({type: CREATE_LINK_SUCCESS, data});
-
-export const openShortLink = (shortUrl) => {
-    return dispatch => {
-        return axios.get('/links/'+shortUrl).then(
-            response => dispatch(fetchSuccess(response.data))
-        );
-    };
-};
 
 export const createLink = link => {
     return dispatch => {
